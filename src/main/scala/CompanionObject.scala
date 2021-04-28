ab5efbf3994fbf7f9f8e457bbd14e52837b044d8
Hirochon <heacet43@gmail.com>
2021-04-28 13:18:13 +0900

    [add] 4/27の分を追加

---
+++ src/main/scala/CompanionObject.scala
@@ -0,0 +1,16 @@
+object CompanionObject extends App{
+  import scala.math._
+
+  case class Circle(radius: Double) {
+    import Circle._
+    def area: Double = calculateArea(radius)
+  }
+
+  object Circle {
+    private def calculateArea(radius: Double): Double = Pi * pow(radius, 2.0)
+  }
+
+  val circle1 = Circle(5.0)
+
+  println(circle1.area)
+}
---
+++ src/main/scala/TypeDiv.scala
@@ -0,0 +1,29 @@
+object TypeDiv extends App{
+  abstract class Animal {
+    def name: String
+  }
+
+  abstract class Pet extends Animal {}
+
+  class Cat extends Pet {
+    override def name: String = "Cat"
+  }
+
+  class Dog extends Pet {
+    override def name: String = "Dog"
+  }
+
+  class Lion extends Animal {
+    override def name: String = "Lion"
+  }
+
+  class PetContainer[P <: Pet](p: P) {
+    def pet: P = p
+  }
+
+  val dogContainer = new PetContainer[Dog](new Dog)
+  val catContainer = new PetContainer[Cat](new Cat)
+
+  // これはコンパイルされません
+//  val lionContainer = new PetContainer[Lion](new Lion)
+}
